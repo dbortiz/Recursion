3ae43ee2f829f65ef67d09c60ba80df5eed8e567
Me <dbortiz@csu.fullerton.edu>
2017-05-09 15:09:07 -0700

    Uncommented examples: template function works!

--- main.cpp
+++ main.cpp
@@ -7,16 +7,16 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     int iList[6] = {1, 3, 5, 17, 23, 98};
-    //float fList[6] = {0.3, 1.2, 11.3, 17.1, 18.9, 97.6};
-    //char cList[6] = {a, c, f, j, o, z};
+    double fList[6] = {0.3, 1.2, 11.3, 17.1, 18.9, 97.6};
+    char cList[6] = {'a', 'c', 'f', 'j', 'o', 'z'};
 
     int iAns = binary_search(iList, 0, 5, 17);
-    //int fAns = binary_search(fList, 0, 5, ??);
-    //int cAns = binary_search(cList, 0, 5, ??);
+    int fAns = binary_search(fList, 0, 5, 18.9);
+    int cAns = binary_search(cList, 0, 5, 'z');
 
     cout << "Integer answer at index " << iAns << endl;
-    //cout << "Float answer at index " << fAns << endl;
-    //cout << "Char answer at index " << cAns << endl;
+    cout << "Float answer at index " << fAns << endl;
+    cout << "Char answer at index " << cAns << endl;
 
     return 0;
 }
